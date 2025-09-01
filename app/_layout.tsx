75face61b43924e20e0145cedb58a7c45f81c332
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2025-09-01 22:26:44 +0900

    [ADD] Swipe 효과를 위한 제스쳐핸들러 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
 // app/_layout.tsx
 import { CustomAlertProvider } from "@/hooks/useCustomAlert";
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <CustomAlertProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </CustomAlertProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <CustomAlertProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </CustomAlertProvider>
+    </GestureHandlerRootView>
   );
 }
